3702d25aad661662eada099836ee3e5d858911e4
Kevin Chang <kmchang@barracuda.com>
2019-04-01 12:43:56 -0400

    Updated i3config to put things on the correct monitors

---
+++ configs/desktop_workspaces.sh
@@ -0,0 +1,15 @@
+# Move workspaces to the configuration we like for our desktop at Cuda
+i3-msg workspace 2
+i3-msg move workspace to output DP-1-1
+i3-msg workspace 4
+i3-msg move workspace to output DP-1-1
+
+i3-msg workspace 3
+i3-msg move workspace to output DP-1-3
+
+i3-msg workspace 9
+i3-msg move workspace to output eDP-1
+i3-msg workspace 10
+i3-msg move workspace to output eDP-1
+i3-msg workspace 1
+i3-msg move workspace to output eDP-1
